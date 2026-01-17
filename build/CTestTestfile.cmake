51c5de3bd13590cc7600820c7d7a9987e89b4f5f
elross913 <guillaume.rossi.913@outlook.com>
2026-01-17 16:00:03 +0100

    Test bin

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /Users/guillaumerossi/Downloads/Embedded-RingBuffer
+# Build directory: /Users/guillaumerossi/Downloads/Embedded-RingBuffer/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(BufferTests "/Users/guillaumerossi/Downloads/Embedded-RingBuffer/build/unit_tests")
+set_tests_properties(BufferTests PROPERTIES  _BACKTRACE_TRIPLES "/Users/guillaumerossi/Downloads/Embedded-RingBuffer/CMakeLists.txt;15;add_test;/Users/guillaumerossi/Downloads/Embedded-RingBuffer/CMakeLists.txt;0;")
